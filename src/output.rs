f8faf07a69a17a34f33bac5eafeacfdac801e427
deepneko <deep.inu@gmail.com>
2022-07-19 22:35:54 +0900

    add sdl.

--- src/output.rs
+++ src/output.rs
@@ -4,6 +4,7 @@ use sdl2::{Sdl, VideoSubsystem};
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use std::time::Duration;
 
 pub struct Output {
     pub sdl_context: Sdl,
@@ -36,6 +37,17 @@ impl Output {
     pub fn write_screen(&mut self) {
         self.canvas.set_draw_color(Color::RGB(100, 255, 255));
         self.canvas.clear();
+
+        let mut event_pump = self.sdl_context.event_pump().unwrap();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    std::process::exit(0);
+                },
+                _ => {}
+            }
+        }
         self.canvas.present();
     }
 }
