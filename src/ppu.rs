41a2544abf597b2799aaa18eec54f86620d04723
deepneko <deep.inu@gmail.com>
2022-08-22 22:45:03 +0900

    started to implement ppu.

--- src/ppu.rs
+++ src/ppu.rs
@@ -1,19 +1,27 @@
 pub struct PPU {
-
+    ram: [u8; 0x4000],
 }
 
 impl PPU {
     pub fn new() -> Self {
         PPU {
-
+            ram: [0; 0x4000],
         }
     }
 
     pub fn read(&self, addr: u16) -> u8 {
-        0
+        self.ram[addr as usize]
     }
 
     pub fn write(&mut self, addr: u16, dat: u8) {
+        self.ram[addr as usize] = dat;
+    }
 
+    pub fn write_scanline(&mut self, line: u8) {
+
+    }
+
+    pub fn wirte_sprites(&mut self) {
+        
     }
 }
